d2168e3637e4d44a85ed81a2cc09531e9179ed39
Myron Liu <myronliu@berkeley.edu>
2020-01-03 14:36:17 -0800

    finished website1.0

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const redis = require('redis');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const cors = require('cors')
-const mysql = require('mysql');
-const async = require('async');
-const path = require('path');
-const database = require('./database')
-
-const router = express.Router()
+//const database = require('./database')
 const app = express();
-const redisStore = require('connect-redis')(session);
-const client = redis.createClient()
 const ONE_DAY = 1000 * 60 * 60 * 24
 
 const {
@@ -21,14 +12,9 @@ const {
   SESS_NAME = 'sid',
   SESS_SECRET = 'magic!',
 } = process.env
-const IN_PROD = NODE_ENV === 'production'
 
-const ACTIONS = {
-  signin: 'signin',
-  signup: 'signout',
-  logout: 'logout',
-}
-const db = database.runDatabase()
+
+// const db = database.runDatabase()
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -44,66 +30,32 @@ var allowCrossDomain = function(req, res, next) {
 }
 
 
-// Helpers
-function checkUsername(username) {
-  var err = false;
-  if (username.length == 0) {
-    err = 'No username inputted'
-  } else if (username.length > 20) {
-    err = 'Username can be at most 20 characters'
-  } else if (!username.match(/^[\w]+$/)) {
-    err = 'Username must be alphanumeric'
-  }
-  return err
-}
-
-function checkPassword(password) {
-  var err = false;
-  if (!password || password.length < 6) {
-    err = 'Password must be at least 6 characters'
-  }
-  return err
-}
-
-// Server handling
-app.use(session({
-  name: SESS_NAME,
-  resave: false,
-  saveUninitialized: false,
-  store: new redisStore({ host: 'localhost', port: 6379, client: client, ttl: 260}),
-  secret: SESS_SECRET,
-  cookie: {
-    maxAge: SESS_LIFETIME,
-    sameSite: true,
-    secure: IN_PROD,
-  },
-}))
+// Server handlin
  
 app.use(allowCrossDomain);
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/views'));
-
+app.use(express.static(__dirname + '/pages'));
 
 // Resources
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '.ndex.html');
 })
 
-app.get('/home', (req, res) => {
-  res.sendFile('home.html')
+app.get('/bio', (req, res) => {
+  res.sendFile(__dirname + 'bio.html')
 })
 
-app.get('/highscores', (req, res) => {
-  res.sendFile('highscores.html')
-})
-
-app.get('/statistics', (req, res) => {
-  res.sendFile('statistics.html')
+app.get('/portfolio', (req, res) => {
+  res.sendFile(__dirname + 'portfolio.html')
 })
 
 app.get('/contact', (req, res) => {
-  res.sendFile('contact.html')
+  res.sendFile(__dirname + 'contact.html')
+})
+
+app.get('/resume', (req, res) => {
+  res.sendFile(__dirname +'resume.pdf')
 })
 
 
